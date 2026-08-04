d8a94783a4e1dec1f188a1a9476a5183941afa61
Guillermo Armando Campuzano Arrechua <gcampuzano@compuequip.com>
2026-08-03 21:34:38 -0500

    feat(backend): capas de modulos extra por usuario

---
+++ CapaEntidad/EntUsuarioMenuBusqueda.cs
@@ -0,0 +1,12 @@
+namespace CapaEntidad
+{
+    public class EntUsuarioMenuBusqueda
+    {
+        public string Cod_Usuario { get; set; }
+        public string Nom_Usuario { get; set; }
+        public string Cedula { get; set; }
+        public long Id_Perfil { get; set; }
+        public string NombrePerfil { get; set; }
+        public int TotalExtras { get; set; }
+    }
+}
